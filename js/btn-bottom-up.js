21ba77d12e5fbdddd8507b3683403f21ade1dae9
Afkeomre <voronk0va.ir@yandex.ru>
2024-08-29 14:29:51 +0300

    fix button

--- js/btn-bottom-up.js
+++ js/btn-bottom-up.js
@@ -7,32 +7,33 @@ window.addEventListener('scroll', turnBtn);
 btnBottomUp.addEventListener('click', scrollPage);
 
 let position = window.scrollY;
+
 function turnBtn() {
-   let scroll = window.scrollY;
-   if (
-      position > scroll ||
-      Math.max(
-         document.body.scrollHeight,
-         document.documentElement.scrollHeight,
-         document.body.offsetHeight,
-         document.documentElement.offsetHeight,
-         document.body.clientHeight,
-         document.documentElement.clientHeight
-      ) -
-         Math.ceil(window.innerHeight + scroll) <=
-         1
-   ) {
-      btnBottomUp.classList.add('btn__bottom-up_up');
-      position = scroll;
-   }
-   if (position < scroll || scroll === 0) {
-      btnBottomUp.classList.remove('btn__bottom-up_up');
-      position = scroll;
-   }
+  let scroll = window.scrollY;
+  if (
+    position > scroll ||
+    Math.max(
+      document.body.scrollHeight,
+      document.documentElement.scrollHeight,
+      document.body.offsetHeight,
+      document.documentElement.offsetHeight,
+      document.body.clientHeight,
+      document.documentElement.clientHeight
+    ) -
+      Math.ceil(window.innerHeight + scroll) <=
+      1
+  ) {
+    btnBottomUp.classList.add('btn__bottom-up_up');
+    position = scroll;
+  }
+  if (position < scroll || scroll === 0) {
+    btnBottomUp.classList.remove('btn__bottom-up_up');
+    position = scroll;
+  }
 }
 
 function scrollPage() {
-   btnBottomUp.classList.contains('btn__bottom-up_up')
-      ? header.scrollIntoView()
-      : footer.scrollIntoView();
+  btnBottomUp.classList.contains('btn__bottom-up_up')
+    ? header.scrollIntoView()
+    : footer.scrollIntoView(false);
 }
